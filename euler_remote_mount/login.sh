12f5d98dfbd17110fa9bb2172dae300d19c4fd9b
Sebastian Lochmann <slochmann@ethz.ch>
2026-03-24 10:08:19 +0100

    add ssh keygen login

--- euler_remote_mount/login.sh
+++ euler_remote_mount/login.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-source .env
-sshfs slochmann@euler.ethz.ch:/cluster/home/slochmann /home/sebi/MSc/3.Sem/semester_thesis/local-git/semester_project_autobrep/euler_remote/euler_project -o ssh_command='sshpass -p '$PASS' ssh' &
-sshfs slochmann@euler.ethz.ch:/cluster/scratch/slochmann /home/sebi/MSc/3.Sem/semester_thesis/local-git/semester_project_autobrep/euler_remote/euler_scratch -o ssh_command='sshpass -p '$PASS' ssh' &
-sshpass -p $PASS ssh  slochmann@euler.ethz.ch
+KEY_PATH="$HOME/.ssh/id_ed25519"
+MOUNT_HOME="/home/sebi/MSc/3.Sem/semester_thesis/local-git/semester_project_autobrep/euler_remote_mount/home"
+MOUNT_SCRATCH="/home/sebi/MSc/3.Sem/semester_thesis/local-git/semester_project_autobrep/euler_remote_mount/scratch"
+
+# Ensure mount directories exist and are empty
+mkdir -p "$MOUNT_HOME" "$MOUNT_SCRATCH"
+
+# Mount home and scratch with SSH key
+sshfs -o 'ssh_command=ssh -i '"$KEY_PATH" slochmann@euler.ethz.ch:/cluster/home/slochmann "$MOUNT_HOME" &
+sshfs -o 'ssh_command=ssh -i '"$KEY_PATH" slochmann@euler.ethz.ch:/cluster/scratch/slochmann "$MOUNT_SCRATCH" &
+
+# Connect terminal via SSH
+ssh -i "$KEY_PATH" slochmann@euler.ethz.ch
---
+++ euler_remote_mount/ssh-keygen.sh
@@ -0,0 +1,2 @@
+ ssh-keygen -t ed25519 -C 'slochmann@ethz.ch'
+ ssh-copy-id slochmann@euler.ethz.ch
